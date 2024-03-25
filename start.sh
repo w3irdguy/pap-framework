5c3f422a20447b452fa9f406126604590e6016a9
f55839359302s93590293o5960c95382i583e3897528t3237y <127699732+z4kc@users.noreply.github.com>
2024-03-25 06:54:04 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,19 @@
+read -p "Do you want install (y/n) > " resp
+if [ "$resp" == "y" ]
+then
+	sudo apt install ruby
+	gem install tty-prompt
+	gem install pastel
+	sudo chmod +x papinput
+	sudo mv papinput /bin/
+	mv usp.rb $HOME
+	
+	echo -e "type\033[0;49;91m papinput\033[m to start the console"
+	
+elif [ "$resp" == "n" ]
+then
+	echo -e "\033[0;49;91m bye bye\033[m"
+	
+else
+	echo "..."
+fi
